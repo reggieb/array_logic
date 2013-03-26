c625c31052f7f9e0c90ea7b0ba2cd01d1bf01f06
Rob Nichols <git@nicholshayes.co.uk>
2013-03-26 16:34:03 +0000

    Tests for instance method using ?

--- test/array_logic/rule_test.rb
+++ test/array_logic/rule_test.rb
@@ -231,7 +231,7 @@ module ArrayLogic
     end
     
     def test_count_with_method_that_can_return_nil
-      @rule.rule = 'count(:id_odd) == 1'
+      @rule.rule = 'count(:id_odd?) == 1'
       assert_thing_match([1, 2], @rule)
       assert_no_thing_match([1, 2, 3], @rule)
       assert_thing_match([3], @rule)
